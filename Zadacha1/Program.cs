b1ccf29357ed06d67863b5778751c7e7bdf34829
Boss <bagir61@mail.ru>
2024-10-31 13:32:10 +0300

    repa03

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Zadacha1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z;
+            x = -10;
+            z = 5;
+            y = Math.Abs(x); //Возвращает модуль числа значение 
+            Console.WriteLine("Модуль  числа  = "+ y); 
+            /*y = Math.Acos(z);  // Возвращает косинус угла  
+            Console.WriteLine("Косинус угла в радианах = { 0,10:f3} ", y); 
+            y = Math.Asin(z); //   Возвращает синус угла  
+            Console.WriteLine("Синус  угла  в  радианах  = { 0,10:f3}" , y); 
+            y = Math.Ceiling(x);    //Возвращает  наименьшее  целое число больше переданного в параметре
+            Console.WriteLine("Наименьшее, больше заданного =  { 0,10:f3}" ,y); 
+            y = Math.Exp(x); //  Возвращает число E в степени (Степень передается в качествепараметра)
+            Console.WriteLine("е  в  степени  х  = { 0,10:f3}", y); 
+            y = Math.Log(z);    //Возвращает  логарифм.  Можно  указать основание логарифма
+            Console.WriteLine("Логарифм Z = {0,10:f3}", y);
+            y = Math.Log10(z);  //  Возвращает 10-ный логарифм
+            Console.WriteLine("Десятичный  логарифм  Z  = { 0,10:f3} " ,y); 
+            y = Math.Max(x, z); //  Возвращает максимум между двумя переданными параметрами
+            Console.WriteLine("Максимальное  из  двух  чисел =   { 0,10:f3}" , y); 
+            y = Math.Min(x, z); //   Возвращает минимум между двумя переданными параметрами
+            Console.WriteLine("Минимальное из двух чисел = { 0,10:f3} " , y); 
+            y = Math.Pow(x, 3);  //  Возвращает степень числа. Первый параметр число, второй – степень.
+            Console.WriteLine("x  в  кубе  =    {0,10:f3}", y);
+            y = Math.Round(x); //  Округляет число
+            Console.WriteLine("Округлить  до  целого  = { 0,10:f3}" , y); 
+            y = Math.Sqrt(z);  //    Возвращает  корень  числа  переданного в качестве параметра.
+            Console.WriteLine("Корень  числа  ={0,10:f3}", y);*/
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
